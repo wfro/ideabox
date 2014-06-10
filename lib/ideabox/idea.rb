d3f645007f3c85866777255e322cd58521648d3b
Will Faurot <wfaurot@gmail.com>
2014-06-10 09:35:57 -0600

    Initial commit

---
+++ lib/ideabox/idea.rb
@@ -0,0 +1,3 @@
+class Idea
+  
+end
---
+++ test/ideabox/idea_test.rb
@@ -0,0 +1,12 @@
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ideabox/idea'
+
+class IdeaTest < Minitest::Test
+  def test_basic_idea
+    idea = Idea.new("title", "description")
+    assert_equal "title", idea.title
+    assert_equal "description", idea.description
+  end
+end
